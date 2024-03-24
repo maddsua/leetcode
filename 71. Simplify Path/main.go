566b2aa4c7fed7c9e65f2393371b57f78b90b74a
maddsua <maddsuadev@proton.me>
2024-03-24 10:23:47 +0200

    Create main.go

---
+++ 71. Simplify Path/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "regexp"
+
+func simplifyPath(path string) string {
+
+	if path[len(path)-1] != '/' {
+		path += "/"
+	}
+
+	result := regexp.MustCompile("\\/\\.\\/").ReplaceAll([]byte(path), []byte("/"))
+
+	for dl := 0; dl != len(result); {
+		dl = len(result)
+		result = regexp.MustCompile("\\/[^\\/]+\\/\\.{2}\\/").ReplaceAll(result, []byte("/"))
+	}
+
+	result = regexp.MustCompile("\\/\\.+\\/").ReplaceAll(result, []byte("/"))
+
+	result = regexp.MustCompile("\\/{2,}").ReplaceAll(result, []byte("/"))
+
+	if result[len(result)-1] == '/' && len(result) > 1 {
+		result = result[:len(result)-1]
+	}
+
+	return string(result)
+}
+
+func main() {
+	//println(simplifyPath("/home//foo/../test/../another/"))
+	println(simplifyPath("/a/./b/../../c/"))
+	println(simplifyPath("/a//b////c/d//././/.."))
+}
