5fb21f757cf50687d7bca502e814f8586a600fb7
maddsua <maddsuadev@proton.me>
2024-03-24 02:21:56 +0200

    Update main.go

--- 67. Add Binary/main.go
+++ 67. Add Binary/main.go
@@ -2,64 +2,67 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strconv"
 )
-
-func parseBuffer(str string, width int64) []byte {
-
-	byteWidth := width / 8
-	if (width % 8) > 0 {
-		byteWidth++
-	}
-
-	result := make([]byte, byteWidth)
-
-	for i := len(str) - 1; i >= 0; i-- {
-
-		idx := i
-		byteIdx := idx / 8
-		bitIdx := (idx % 8)
-
-		var setBit int8
-		if str[i] == '1' {
-			setBit = 1
-		}
-
-		//println("bitIdx", bitIdx, setBit, string(str[i]))
-
-		result[byteIdx] |= byte(setBit << bitIdx)
-	}
-
-	return result
-}
-
-func printByteAsBits(data []byte) {
-	for _, b := range data {
-		for i := 7; i >= 0; i-- {
-			bit := (b >> uint(i)) & 1
-			fmt.Print(strconv.FormatInt(int64(bit), 2))
-		}
-		fmt.Print(" ")
-	}
-	fmt.Println()
-}
 
 func addBinary(a string, b string) string {
 
-	bitWidth := math.Max(float64(len(a)), float64(len(b)))
+	bitWidth := len(a)
+	if len(b) > bitWidth {
+		bitWidth = len(b)
+		a = fmt.Sprintf("%*s", bitWidth, a)
+	} else if len(b) < bitWidth {
+		b = fmt.Sprintf("%0*s", bitWidth, b)
+	}
 
-	printByteAsBits(parseBuffer(a, int64(bitWidth)))
-	printByteAsBits(parseBuffer(b, int64(bitWidth)))
+	var carryflag bool
+	var temp []byte
 
-	aNum, _ := strconv.ParseInt(a, 2, 32)
-	bNum, _ := strconv.ParseInt(b, 2, 32)
-	return strconv.FormatInt(aNum+bNum, 2)
+	for i := bitWidth - 1; i >= 0; i-- {
+
+		var sum int
+		if carryflag {
+			sum = 1
+		}
+
+		if a[i] == '1' {
+			sum++
+		}
+		if b[i] == '1' {
+			sum++
+		}
+
+		if sum < 2 {
+
+			if sum == 0 {
+				temp = append(temp, '0')
+			} else {
+				temp = append(temp, '1')
+			}
+
+			carryflag = false
+
+		} else {
+			temp = append(temp, '0')
+			carryflag = true
+		}
+	}
+
+	if carryflag {
+		temp = append(temp, '1')
+	}
+
+	var result []byte
+
+	for i := len(temp) - 1; i >= 0; i-- {
+		result = append(result, temp[i])
+	}
+
+	return string(result)
 }
 
 func main() {
-	//addBinary("10", "1")
-	//addBinary("11", "1")
-	addBinary("1010", "1011")
+	//println(addBinary("10", "1"))
+	//println(addBinary("11", "1"))
+	println(addBinary("1010", "1011"))
 	//println(addBinary("1010", "1011"))
 }
